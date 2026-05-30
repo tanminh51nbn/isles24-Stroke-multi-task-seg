01448f71ae562a0769c90720c4eb4b523e3c6683
tanminh51nbn <tanminh51nbn@gmail.com>
2026-05-31 00:11:28 +0700

    fix(lvo): rename remaining f1_lvo variables to dice_lvo across all files to fix ImportError

--- src/training/callbacks.py
+++ src/training/callbacks.py
@@ -96,7 +96,7 @@ class ModelCheckpoint:
 
         comp    = metrics["composite"]
         lesion  = metrics["dice_lesion"]
-        lvo     = metrics["f1_lvo"]
+        lvo     = metrics["dice_lvo"]
 
         if self.save_overall and comp > self.best_composite:
             self.best_composite = comp
